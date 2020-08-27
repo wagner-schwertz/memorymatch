ec76c3f051449daab315ac2186ed68e17190adc3
Wagner Schwertz dos Santos <wagner.santos@ntconsult.com.br>
2020-08-26 21:44:02 -0300

    Updates

---
+++ precache-manifest.3233c7a838b65fd522eed1906aa3b582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c007fab02fb75d2852bd66b7b5057e8",
+    "url": "/memorymatch/index.html"
+  },
+  {
+    "revision": "eaf1b9fba46c8ecddfde",
+    "url": "/memorymatch/static/js/2.db8a23a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memorymatch/static/js/2.db8a23a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92839a84f246816d7499",
+    "url": "/memorymatch/static/js/main.40ee4838.chunk.js"
+  },
+  {
+    "revision": "3fab658f6485236043a2",
+    "url": "/memorymatch/static/js/runtime-main.fd8a6462.js"
+  }
+]);
